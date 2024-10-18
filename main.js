fce89a2ad3a0c4d0745d0c6e13b04493389e330b
Nurel077 <nurelmalabaev95@gmail.com>
2024-10-18 22:30:26 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+// // Получаем элементы
+// const modal = document.getElementById("myModal");
+// const openModalBtn = document.getElementById("openModalBtn");
+// const closeBtn = document.querySelector(".close");
+
+// // Открытие модального окна при клике на кнопку
+// openModalBtn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// // Закрытие модального окна при клике на крестик
+// closeBtn.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// // Закрытие модального окна при клике вне его
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+// 
+const carousel = new bootstrap.Carousel('#myCarousel')
+
+
